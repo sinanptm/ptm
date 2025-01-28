a035d9ee846cda11726b6cf952bf4518f91b5a6a
Muhammed Sinan <muhammedsinan7749@gmail.com>
2025-01-28 05:33:36 +0530

    site map creation updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://sinanptm.vercel.app', 
-    generateRobotsTxt: true,
+    siteUrl: 'https://ptmkalari.vercel.app', 
+    generateRobotsTxt: true, 
+    generateIndexSitemap: true, 
+    changefreq: 'weekly', 
+    priority: 0.7, 
     exclude: [],
-};
+    robotsTxtOptions: {
+      policies: [
+        {
+          userAgent: '*',
+          allow: '/', 
+        },
+      ],
+      additionalSitemaps: [
+        'https://ptmkalari.vercel.app/sitemap-0.xml', 
+      ],
+    },
+  };
+  
